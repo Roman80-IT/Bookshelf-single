232970c003dc2d1d2fc370bdf672ac1ada378187
Roman <jrj80mail@gmail.com>
2023-08-30 18:42:45 +0300

    js fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import './js/fetch-categories.js'; // отримання списку категорій
-import './js/fetch-best-sellers.js'; // отримання списку найпопулярніших книг
-import './js/fetch-books-by-category.js'; //отримання книг за категорією
-import './js/theme-switcher.js'; // перемикач тем
-import './js/mobile-menu.js';
-import './js/support-ukraine.js';
-import './js/authorization.js';
-import './js/user-bar.js'; //панель користувача
-import './js/book-modal.js'; // модальне вікно книги
-import './js/logout.js'; // вихід з облікового запису
+// import './js/fetch-categories.js'; // отримання списку категорій
+// import './js/fetch-best-sellers.js'; // отримання списку найпопулярніших книг
+// import './js/fetch-books-by-category.js'; //отримання книг за категорією
+// import './js/theme-switcher.js'; // перемикач тем
+// import './js/mobile-menu.js';
+// import './js/support-ukraine.js';
+// import './js/authorization.js';
+// import './js/user-bar.js'; //панель користувача
+// import './js/book-modal.js'; // модальне вікно книги
+// import './js/logout.js'; // вихід з облікового запису
--- shopping-list.js
+++ shopping-list.js
@@ -1,7 +1,7 @@
-import './js/theme-switcher.js';
-import './js/mobile-menu.js';
-import './js/support-ukraine.js';
-import './js/authorization.js';
-import './js/user-bar.js';
-import './js/book-modal.js';
-import './js/logout.js';
+// import './js/theme-switcher.js';
+// import './js/mobile-menu.js';
+// import './js/support-ukraine.js';
+// import './js/authorization.js';
+// import './js/user-bar.js';
+// import './js/book-modal.js';
+// import './js/logout.js';
